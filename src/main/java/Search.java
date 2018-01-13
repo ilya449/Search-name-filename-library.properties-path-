17998f63e6ef36cc272e96789ef5c52a1bdc370f
Ilya <ilya03449@gmail.com>
2018-01-13 15:58:20 +0200

    Search "name filename:library.properties path:/"

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,17 +1,33 @@
 import org.kohsuke.github.*;
+import java.io.FileWriter;
 
+/*
+ownername, name, defbranch, starscount
+*/
 public class Search {
     public static void main(String[] args) throws Exception {
-        GitHub github = GitHub.connectUsingPassword("iluhachernyshov@ukr.net","63h0dUcONW121q");
-        /*
-        int i=1;
-        for (GHContent cont : github.searchContent().q("name").filename("library.properties").path("/").list())
+        FileWriter file = new FileWriter("D:\\test.txt");
+        GitHub github = GitHub.connect();
+        GHRepository repo = new GHRepository();
+        String s;
+        int i = 0;
+        for(GHContent cont : github.searchContent().q("name").filename("library.properties").path("/").list())
         {
-            System.out.println(". "+cont.getHtmlUrl());
+            //System.out.println(". " + cont.getHtmlUrl());
+            s = cont.getHtmlUrl();
+            s = s.substring(s.indexOf("github.com/") + 11, s.indexOf("/blob/"));
+            //System.out.println(s);
+            repo = github.getRepository(s);
+            System.out.print(repo.getOwnerName() + ", ");
+            System.out.print(repo.getName() + ", ");
+            System.out.print(repo.getDefaultBranch() + ", ");
+            System.out.print(repo.getStargazersCount()+"\n");
+            file.write(repo.getOwnerName() + ", "+repo.getName() + ", "+repo.getDefaultBranch() + ", "+repo.getStargazersCount());
+            file.write(System.getProperty("line.separator"));
+            Thread.sleep(2001);
             i++;
-            if (i==6) break;
+            if (i == 10) break;
         }
-        */
-        //System.out.println(github.searchContent().q("name").filename("library.properties").path("/").list().getTotalCount());
+        file.close();
     }
 }
